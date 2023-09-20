e0db68f7005ba2c0c385be158c4998448b48bf46
Jonathan Liu <jonathan.tzuchi.liu@gmail.com>
2023-09-20 22:09:26 +0800

    v2

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import io
 import mic
+import time
 import pygame
 import openai
 
 from gtts import gTTS
 from secret_key import openai_key
+
+start_time = time.time()
 
 openai.api_key = openai_key
 
@@ -17,28 +20,52 @@ def chat(question, personality):
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=messages,
-        max_tokens=100
+        max_tokens=1000,
+        # stream=True
     )
 
     return response.choices[0].message['content']
 
-while True:
-    audio = mic.record_audio()
-    text = mic.transcribe_forever(audio)
-    print("You said:" + text)
-    final = chat(text, "請你用繁體中文回答我這個問題")
-    print(final)
+def main():
+    while True:
+        audio = mic.record_audio()
+        text, whisper_time = mic.transcribe_forever(audio)
+        print("\nWhisper's Transcribe Result:" + text)
+        print("Whisper Time Taken: {:.2f} seconds".format(whisper_time))
 
-    tts = gTTS(final, lang="zh-tw")
+        print("\nMessage Sent to ChatGPT. Generating Response...")
+        
+        start_time = time.time()
+        responses = chat(text, "請你用繁體中文回答我這個問題")
+        end_time = time.time()
 
-    # Save the TTS audio as an in-memory file
-    audio_data = io.BytesIO()
-    tts.write_to_fp(audio_data)
-    audio_data.seek(0)  # Move to the beginning of the in-memory audio data
+        print("\nChatGPT Response:\n\n",responses)
+        print("\nChatGPT Time Taken: {:.2f} seconds".format(end_time - start_time))
+
+        print("\nSpeaking...")
+
+        # for response in responses:
+        #     generated_text = response['choices'][0]['delta']["content"]
+        #     decoded_text = generated_text
+        #     print(decoded_text, end="")
+        
+        # print("\n\n\n\n")
+
+        tts = gTTS(responses, lang="zh-tw")
+
+        # Save the TTS audio as an in-memory file
+        audio_data = io.BytesIO()
+        tts.write_to_fp(audio_data)
+        audio_data.seek(0)  # Move to the beginning of the in-memory audio data
 
 
-    pygame.mixer.init()
-    pygame.mixer.music.load(audio_data)
-    pygame.mixer.music.play()
-    while pygame.mixer.music.get_busy():
-        pygame.time.delay(100)
+        pygame.mixer.init()
+        pygame.mixer.music.load(audio_data)
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            pygame.time.delay(100)
+
+        
+
+if __name__ == "__main__":
+    main()
--- mic.py
+++ mic.py
@@ -5,6 +5,9 @@ from pydub import AudioSegment
 import numpy as np
 import whisper
 
+import asyncio
+import time
+
 def record_audio():
     #load the speech recognizer and set the initial energy threshold and pause threshold
     r = sr.Recognizer()
@@ -13,24 +16,33 @@ def record_audio():
     r.dynamic_energy_threshold = False
 
     with sr.Microphone(sample_rate=16000) as source:
-        print("Say something!")
+        print(" \n\n\n\n\n\n\n\n\n\n\n ============================== \n\nSay something!")
         i = 0
         #get and save audio to wav file
         audio = r.listen(source)
         torch_audio = torch.from_numpy(np.frombuffer(audio.get_raw_data(), np.int16).flatten().astype(np.float32) / 32768.0)
         audio_data = torch_audio
 
+        print("\nMessage sent to Whisper. Transcribing...")
+
         return audio_data
 
 def transcribe_forever(audio):
+
+    start_time = time.time()
+
     audio_data = audio
     audio_model = whisper.load_model("base")
     result = audio_model.transcribe(audio_data,language='zh') #Chinese model
+
+    end_time = time.time()
+
+    whisper_time = end_time - start_time
 
     #result = audio_model.transcribe(audio_data)
 
 
     predicted_text = result["text"]
-    return predicted_text
+    return predicted_text, whisper_time
 
     #result_queue.put_nowait(result) #Complete result
